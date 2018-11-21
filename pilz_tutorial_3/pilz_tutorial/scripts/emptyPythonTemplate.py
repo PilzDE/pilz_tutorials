5932a3332245c3c2e6271cc5505ee4c04bc5a44c
Jonathan Gruner <j.gruner@pilz.de>
2018-11-21 13:04:57 +0100

    Tutorial 2 in Tutorial 2 und 3 gesplittet

---
+++ pilz_tutorial_3/pilz_tutorial/scripts/emptyPythonTemplate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+
+from geometry_msgs.msg import Pose, Point
+from pilz_robot_programming import *
+import math
+import rospy
+
+__REQUIRED_API_VERSION__ = "0"  #API version
+__ROBOT_VELOCITY__ = 0.5        #velocity of the robot
+
+#main program
+def start_program():
+    pass
+
+if __name__ == "__main__":
+    #init a rosnode
+    rospy.init_node('robot_program_node')
+
+    #initialisation
+    r = Robot(__REQUIRED_API_VERSION__)  #instance of the robot
+
+    #start the main program
+    start_program()
--- pilz_tutorial_3/pilz_tutorial/scripts/myFirstApplication.py
+++ pilz_tutorial_3/pilz_tutorial/scripts/myFirstApplication.py
@@ -5,7 +5,7 @@ import math
 import rospy
 
 __REQUIRED_API_VERSION__ = "0"  #API version
-__ROBOT_VELOCITY__ = 1          #velocity of the robot
+__ROBOT_VELOCITY__ = 0.5          #velocity of the robot
 
 #main program
 def start_program():
@@ -17,7 +17,7 @@ def start_program():
     pos_work_station = Point(-0.5, 0.1, 0.2)    #cartesian coordinates
     pos_place = Point(-0.1,0.4,0.25)            #cartesian coordinates
     
-    #Spherical coordinates
+    #spherical coordinates
     orientation_pick = from_euler(0, math.radians(180), math.radians(0))
     orientation_work_station = from_euler(0, math.radians(-135), math.radians(90))
     orientation_place = from_euler(0, math.radians(180),  math.radians(90))
@@ -26,20 +26,20 @@ def start_program():
     r.move(Ptp(goal=start_pos, vel_scale=__ROBOT_VELOCITY__))
     
     while(True):
-        #Do infinite loop
-        #Pick the PNOZ
+        #do infinite loop
+        #pick the PNOZ
         r.move(Ptp(goal=Pose(position=pos_pick, orientation=orientation_pick),
             vel_scale = __ROBOT_VELOCITY__, relative=False))
         pick_and_place()
 
-        #Put the PNOZ in a "machine"
+        #put the PNOZ in a "machine"
         r.move(Ptp(goal=Pose(position=pos_work_station,
             orientation=orientation_work_station),vel_scale = __ROBOT_VELOCITY__, relative=False))
         pick_and_place()
         rospy.sleep(1)      # Sleeps for 1 sec (wait until work is finished)
         pick_and_place()
 
-        #Place the PNOZ
+        #place the PNOZ
         r.move(Ptp(goal=Pose(position=pos_place, orientation=orientation_place),
             vel_scale = __ROBOT_VELOCITY__, relative=False))
         pick_and_place()
@@ -47,15 +47,15 @@ def start_program():
 def pick_and_place():
     """pick and place function"""
 
-    #A static velocity from 0.2 is used
-    #The position is given relative to the TCP. 
+    #a static velocity from 0.2 is used
+    #the position is given relative to the TCP. 
     r.move(Ptp(goal=Pose(position=Point(0, 0, 0.1)), reference_frame="prbt_tcp", vel_scale=0.2))
-    rospy.sleep(0.2)    #Pick or Place the PNOZ (close or open the gripper)
+    rospy.sleep(0.2)    #pick or Place the PNOZ (close or open the gripper)
     r.move(Ptp(goal=Pose(position=Point(0, 0, -0.1)), reference_frame="prbt_tcp", vel_scale=0.2))
 
 
 if __name__ == "__main__":
-    # Init a rosnode
+    #init a rosnode
     rospy.init_node('robot_program_node')
 
     #initialisation
