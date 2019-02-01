aec37a630d5ec135ebde66be2a1426c52347e274
Jonathan Gruner <j.gruner@pilz.de>
2019-02-01 10:59:34 +0100

    MERGE

--- pilz_tutorial_3/pilz_tutorial/scripts/myFirstApplication.py
+++ pilz_tutorial_3/pilz_tutorial/scripts/myFirstApplication.py
@@ -4,31 +4,21 @@ from pilz_robot_programming import *
 import math
 import rospy
 
-__REQUIRED_API_VERSION__ = "1"    # API version
-__ROBOT_VELOCITY__ = 0.5          # velocity of the robot
+__REQUIRED_API_VERSION__ = "1"  # API version
+__ROBOT_VELOCITY__ = 0.5        # velocity of the robot
 
 # main program
 def start_program():
 
     # define the positions:
-    joint_goal = [1.49, -0.54, 1.09, 0.05, 0.91,-1.67]  # Use joint values for the first position
-    cartesian_goal=Pose(position=Point(0, 0, 0.1), orientation=from_euler(0, math.radians(180), math.radians(0))) # Use cartesian coordinates for another position
-    rospy.loginfo("Start loop") # log
-        
-        rospy.loginfo("Move to pick position") # log
-        rospy.loginfo("Pick movement") # log
-        rospy.loginfo("Move to virtual machine") # log
-        rospy.loginfo("Place PNOZ in machine") # log
-        rospy.loginfo("Pick PNOZ from machine") # log
-        rospy.loginfo("Move to place position") # log
-        rospy.loginfo("Place movement") # log
+    joint_goal = [1.49, -0.54, 1.09, 0.05, 0.91, -1.67]  # Use joint values for the first position
+    cartesian_goal=Pose(position=Point(-0.04, -0.53, 0.13), orientation=from_euler(0, math.radians(180), math.radians(0))) # Use cartesian coordinates for another position
     
     # Move to start point with joint values to avoid random trajectory
     r.move(Ptp(goal=joint_goal, vel_scale=__ROBOT_VELOCITY__))
-
-    #Move to the second position
+    
+    # Move to the second position
     r.move(Ptp(goal=cartesian_goal, vel_scale=__ROBOT_VELOCITY__))
-    rospy.loginfo("Open/Close the gripper") # log
     
     
 if __name__ == "__main__":
